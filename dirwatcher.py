f603dfe08aed179f015fa076cdd96c23c5acabfc
Marcel Cornett <marcornett@gmail.com>
2020-09-28 13:59:32 -0400

    Made function that creates command line parser object. Accepts directory, extension type, interval, and magic word to search. Used this function in main to make respective argument variables.

--- dirwatcher.py
+++ dirwatcher.py
@@ -3,14 +3,20 @@
 Dirwatcher - A long-running program
 """
 
-__author__ = "???"
+__author__ = "marcornett"
 
 import sys
+import logging
+import argparse
 
 
 def search_for_magic(filename, start_line, magic_string):
     # Your code here
     return
+
+# Do break up your code into small functions such as
+# scan_single_file(), detect_added_files(),
+# detect_removed_files(), and watch_directory().
 
 
 def watch_directory(path, magic_string, extension, interval):
@@ -19,8 +25,18 @@ def watch_directory(path, magic_string, extension, interval):
 
 
 def create_parser():
-    # Your code here
-    return
+    """Creates command line parser object to accept one argument."""
+    parser = argparse.ArgumentParser()
+    # todo if no directory log dirtory XXXX does not exist
+    parser.add_argument('-dir', '--directory', help='Directory to watch.')
+    parser.add_argument('-ext', '--extension',
+                        help='Extension type to look for.')
+    # todo polling interval default to 1.0 seconds
+    parser.add_argument('-int', '--interval',
+                        help='Polling interval. Default 1.0 seconds')
+    parser.add_argument('magic', help='Magic text to search for')
+
+    return parser
 
 
 def signal_handler(sig_num, frame):
@@ -29,8 +45,24 @@ def signal_handler(sig_num, frame):
 
 
 def main(args):
-    # Your code here
-    return
+    """Main driver code for dirwatcher."""
+    # logging.basicConfig(level=logging.INFO)
+    # logging.info("args:", args)
+    parser = create_parser()
+
+    ns = parser.parse_args(args)
+
+    if not ns:
+        # TODO find out if sys.exit is allowed here
+        parser.print_usage()
+        sys.exit(1)
+
+    directory = ns.directory
+    extension = ns.extension
+    interval = ns.interval
+    magic_word = ns.magic
+
+    # TODO call functions using args
 
 
 if __name__ == '__main__':
